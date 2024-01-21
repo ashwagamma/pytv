de1e5c293e1b608ec4921054f480ffcedf000e71
ashwagamma <126337816+ashwagamma@users.noreply.github.com>
2024-01-21 15:24:12 -0800

    Add files via upload

--- levels_parser_mancini.py
+++ levels_parser_mancini.py
@@ -6,7 +6,8 @@ import login_details
 import re
 import json
 import undetected_chromedriver as uc
-from seleniumbase import Driver
+from time import sleep
+
 
 def process_list_range(list_range):
     new_range = []
@@ -77,8 +78,7 @@ options = uc.ChromeOptions()
 prefs = {"credentials_enable_service": False,
          "profile.password_manager_enabled": False}
 options.add_experimental_option("prefs", prefs)
-# browser = uc.Chrome(use_subprocess=True, options=options)
-browser = Driver(uc=True)
+browser = uc.Chrome(use_subprocess=True, options=options)
 browser.maximize_window()
 
 browser.get('https://tradecompanion.substack.com/')
@@ -86,25 +86,29 @@ browser.maximize_window()
 
 wait = WebDriverWait(browser, 10)
 
-elem_x = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[3]/div/div/div/button"))).click()
-# Sign In button top corner
+sleep(1)
+# No thanks
+browser.find_element("xpath", ".//*[contains(text(), 'No thanks')]").click()
+# Sign in
 browser.find_element("xpath", ".//*[contains(text(), 'Sign in')]").click()
-# Sign in with password link
-browser.find_element(By.XPATH, '/html/body/div/div/div[2]/div[2]/form/div[2]/div/a').click()
-#username
-wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/div[2]/form/div[1]/input"))).send_keys(login_details.substack_username)
-#password
-wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/div[2]/form/input[3]"))).send_keys(login_details.substack_password)
-#sign in button
-wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[2]/div[2]/form/button'))).click()
+# Sign in with password
+browser.find_element("xpath", ".//*[contains(text(), 'Sign in with')]").click()
+
+# username
+wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[name='email']"))).send_keys(login_details.substack_username)
+# password
+wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[name='password']"))).send_keys(login_details.substack_password)
+# continue
+wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[type='submit']"))).click()
 
 # first post
-wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/a"))).click()
+wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/a'))).click()
 
-# wait till page has loaded
+# wait till page has loaded basically
 wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/div[1]/div/article/div[4]/div[1]")))
+# wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/a[1]")))
 
-#flag=input('y/n:')
+# flag=input('y/n:')
 
 with open('mancini.txt', 'w', encoding="utf-8") as f:
     f.write(browser.page_source)
@@ -125,21 +129,16 @@ res = re.findall(reg_str, txt)
 txt2 = browser.page_source
 reg_str = "<li><p>(.*?)</p></li>"
 res2 = re.findall(reg_str, txt)
-idx_sup = -1
-for idx in range(0,len(res2)):
-    val = res2[idx]
-    if str(val).lower().startswith('supports'):
-        if idx_sup == -1:
-            idx_sup = idx
-    print(idx, val)
+for val in res2:
+    print(val)
     #print('res2 val=', val)
 
 #"datePublished":"2022-09-14T14:01:13-07:00","dateModified":"2022-09-14T14:01:13-07:00","isAccessibleForFree"
 date_published = re.findall("\"datePublished\":\"(.*?)\",\"dateModified\"", txt)[0]
 print('date_published=', date_published)
 
-txt_support = res2[idx_sup]
-txt_resistance = res2[idx_sup+2]
+txt_support = res2[0]
+txt_resistance = res2[2]
 
 txt_support = txt_support.replace('4k', '4000')
 txt_support = txt_support.replace('3k', '3000')
